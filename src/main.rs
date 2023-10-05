605836754408d74cef0424b4ed9f2ad1ae2415ec
taisuke101 <uwattotaitai@gmail.com>
2023-10-05 16:23:25 +0900

    fix: レスポンスマッパーにログを出力するように修正

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,25 @@ use std::net::SocketAddr;
 
 use axum::{
     extract::{Path, Query},
+    http::{Method, Uri},
     middleware,
     response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Json, Router,
 };
+use ctx::Ctx;
 use serde::Deserialize;
+use serde_json::json;
 use tower_cookies::{CookieManager, CookieManagerLayer};
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
-use crate::model::ModelController;
 use crate::web::{mw_auth, routes_tickets};
+use crate::{log::log_request, model::ModelController};
 
 mod ctx;
 mod error;
+mod log;
 mod model;
 mod web;
 
@@ -54,11 +59,38 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn main_response_mapper(res: Response) -> Response {
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error" : {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!(" ->> client error_body: {client_error_body}");
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    let client_error = client_status_error.unzip().1;
+    log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
 
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
